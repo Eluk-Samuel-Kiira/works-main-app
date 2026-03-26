66efc338fe4da089b8291139245bcfd0061fb9a9
ssegifred <fredsseginda70@gmail.com>
2026-03-26 15:18:02 -0400

    register routes and update sidebar navigation for all CRUD blades

--- routes/jobs.php
+++ routes/jobs.php
@@ -1,7 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Main\Jobs\{ CompanyController, JobPostsController };
+use App\Http\Controllers\Main\Jobs\{
+    CompanyController,
+    EducationLevelController,
+    ExperienceLevelController,
+    IndustryController,
+    JobCategoryController,
+    JobLocationController,
+    JobPostsController,
+    JobTypeController,
+    SalaryRangeController,
+    UserController,
+};
 
 
 
@@ -10,6 +21,14 @@ Route::middleware('auth')->group(function () {
     Route::resource('job-post', JobPostsController::class);
 
     Route::resource('company', CompanyController::class);
+    Route::resource('industry', IndustryController::class)->only(['index']);
+    Route::resource('job-category', JobCategoryController::class)->only(['index']);
+    Route::resource('job-type', JobTypeController::class)->only(['index']);
+    Route::resource('job-location', JobLocationController::class)->only(['index']);
+    Route::resource('experience-level', ExperienceLevelController::class)->only(['index']);
+    Route::resource('education-level', EducationLevelController::class)->only(['index']);
+    Route::resource('salary-range', SalaryRangeController::class)->only(['index']);
+    Route::resource('user', UserController::class)->only(['index']);
 });
 
 
