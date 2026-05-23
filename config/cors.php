e3c8efb8e600dca8d2a316b551eb93f567f2bc5b
Eluk-Samuel-Kiira <samuelkiiraeluk@gmail.com>
2026-05-23 11:52:22 +0300

    AlL Cv recom

--- config/cors.php
+++ config/cors.php
@@ -8,6 +8,7 @@ return [
         'http://localhost:8001',
         'http://127.0.0.1:8000',
         'http://localhost:8000',
+        'https://stardenaworks.com/',
     ],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
