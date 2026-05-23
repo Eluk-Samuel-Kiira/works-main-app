7c1a75ac89620a1a3c1f8a6651cfce034adf8a48
Eluk-Samuel-Kiira <samuelkiiraeluk@gmail.com>
2026-05-23 12:05:47 +0300

    AlL Cv recom

--- config/cors.php
+++ config/cors.php
@@ -1,15 +1,17 @@
-<?php
+// MAIN APP: config/cors.php
 
 return [
-    'paths' => ['api/*', 'sanctum/csrf-cookie', 'login-register/*'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie', 'auth/*', 'login-register/*'],
     'allowed_methods' => ['*'],
     'allowed_origins' => [
         'http://127.0.0.1:8001',
         'http://localhost:8001',
         'http://127.0.0.1:8000',
         'http://localhost:8000',
-        'https://stardenaworks.com/',
-        'https://ma1n.stardenaworks.com/',
+        'https://stardenaworks.com',          
+        'https://www.stardenaworks.com',      
+        'https://ma1n.stardenaworks.com',      
+        'http://stardenaworks.com',           
     ],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
